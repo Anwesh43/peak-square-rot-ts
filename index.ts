5b46267648cb03fac239d2f02f9e192de2e0e650
Anwesh43 <anweshthecool0@gmail.com>
2021-08-09 22:07:38 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -24,3 +24,44 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawPeakSquareRot(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2 + (w / 2 + size) * sc4, h / 2)
+        context.rotate(Math.PI * 0.5 * sc3)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawLine(context, -size / 2, 0, -size / 2, -size * 0.5 * (1 + j) * sc1)
+            context.restore()
+            context.save()
+            context.scale(1, 1 - 2 * j)
+            DrawingUtil.drawLine(context, -size / 2, -(size * 0.5 * j), -size / 2 + size * sc2, -size * 0.5 * j * (1 + sc2))
+            context.restore()
+        }
+    
+        context.restore()
+    }
+
+    static drawPSRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawPeakSquareRot(context, scale)
+    }
+}
+
