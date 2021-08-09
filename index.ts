68cd2ab4dccab2caaab9dcb8231a5adc7a9e482f
Anwesh43 <anweshthecool0@gmail.com>
2021-08-09 22:22:48 +0530

    adding PSRNode class

--- index.ts
+++ index.ts
@@ -139,3 +139,45 @@ class Animator {
         }
     }
 }
+
+class PSRNode {
+
+    next : PSRNode 
+    prev : PSRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new PSRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawPSRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : PSRNode {
+        var curr : PSRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
