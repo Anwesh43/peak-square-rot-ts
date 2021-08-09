454e7a7d2699da5e11249f952f2259ecbf3bda06
Anwesh43 <anweshthecool0@gmail.com>
2021-08-09 22:26:52 +0530

    adding Renderer class

--- index.ts
+++ index.ts
@@ -181,3 +181,26 @@ class PSRNode {
         return this 
     }
 }
+
+class PeakSquareRot {
+
+    curr : PSRNode = new PSRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
